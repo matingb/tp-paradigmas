e63dac72cb3e55043f48581b31130cd03270af70
Matías García <mgarcia@inrule.com>
2023-05-20 16:52:33 -0300

    inicia el proyecto

---
+++ src/unlam/paradigmas/Main.java
@@ -0,0 +1,34 @@
+package unlam.paradigmas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 	Cargar Información
+			- Usuarios
+			- Atracciones
+			- Paquetes 
+				Hace falta hacer la distinción que plantea el enunciado?
+				No especifica ningun tipo de manejo
+				Se podría únicamente cargar una lista atracciones con un precio final
+				Por ej |valor|atraccion1|atraccion2
+		*/
+		
+		/*
+			Procesar la info
+				Me imagino una clase como Boleteria la cual tiene conocimiento
+				de las atracciones, especialmente del cupo. Y es el encargado de 
+				iterar los usuarios.
+				
+				Por otro lado por cada usuario se genera una Sesion, la cual tendría
+				en el encargado de las interacciones con el usuario, mostrando y priorizando
+				lo que el usuario le pida. Cargar todo lo seleccionado y descontar cupo de lo
+				elegido
+		*/
+		
+		/*
+			Escribir archivo con los resultados.
+		*/
+	}
+
+}
