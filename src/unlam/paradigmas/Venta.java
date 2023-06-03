a66f597bbef8d8aafa044d33cf9bd758055ff42b
Tomas <tomasbeta@outlook.com>
2023-06-03 19:11:03 -0300

    desarrollo de escribirOferta()  y crearArchivo()

--- src/unlam/paradigmas/Venta.java
+++ src/unlam/paradigmas/Venta.java
@@ -12,55 +12,97 @@ public class Venta {
 	private double precioTotal;
 	private double tiempoTotal;
 	private Usuario usuario;
-	
-	Venta(Usuario usuario){
+
+	Venta(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
+
 	public void crearArchivo() {
-		File archivo = new File("./salidas/"+usuario.getNombre()+".out");//chequear si ruta OK
-		
-		//TODO escribir los datos del usuario
+		FileWriter archivo = null;
+		PrintWriter escribirArch = null;
+
+		try {
+			archivo = new FileWriter("./salidas/" + usuario.getNombre() + ".out");// chequear si ruta OK
+			escribirArch = new PrintWriter(archivo);
+
+			// TODO escribir los datos del usuario
+			escribirArch.println("					Resumen Itinerario				");
+			escribirArch.println("-----------------------------------------------------------");
+			escribirArch.println("Visitante: " + usuario.getNombre());
+			escribirArch.println("Presupuesto: " + "$" + usuario.getPresupuesto());
+			escribirArch.println("Tiempo Disponible: " + usuario.getTiempo() + "Hrs");
+			escribirArch.println("Actividad Favorita: " + usuario.getActividadFavorita());
+			escribirArch.println("-----------------------------------------------------------");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (archivo != null) {
+				try {
+					archivo.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
-	
+
 	// escribirOferta() va escribiendo todas las ofertas que acepta el usuario
-	public void escribirOferta(Oferta oferta) { 
-		
-		//TODO escribir en el archivo los datos de la oferta aceptada
-		//invocar a actualizarTiempoyPrecio()
+	public void escribirOferta(Oferta oferta) {
+
+		// TODO escribir en el archivo los datos de la oferta aceptada
+		// invocar a actualizarTiempoyPrecio()
+
+		FileWriter archivo = null;
+		PrintWriter escribirArch = null;
+
+		try {
+			archivo = new FileWriter("./salidas/" + usuario.getNombre() + ".out", true);// chequear si ruta OK
+			escribirArch = new PrintWriter(archivo);
+
+			escribirArch.println(oferta);
+
+			actualizarTiempoyPrecio(oferta);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (archivo != null) {
+				try {
+					archivo.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
-	
-	
+
 	private void actualizarTiempoyPrecio(Oferta oferta) {
-		tiempoTotal+=oferta.getDuracion();
-		precioTotal+=oferta.getPrecio();
+		tiempoTotal += oferta.getDuracion();
+		precioTotal += oferta.getPrecio();
 	}
-	
-	
+
 	public void escribirTotales() {
 		FileWriter archivo = null;
 		PrintWriter printerWriter = null;
-		
+
 		try {
-			archivo = new FileWriter("./salidas/"+usuario.getNombre()+".out");
+			archivo = new FileWriter("./salidas/" + usuario.getNombre() + ".out");
 			printerWriter = new PrintWriter(archivo);
-			printerWriter.println("Total a pagar: $"+precioTotal);
-			printerWriter.println("Tiempo total: "+tiempoTotal+" hrs.");
+			printerWriter.println("Total a pagar: $" + precioTotal);
+			printerWriter.println("Tiempo total: " + tiempoTotal + " hrs.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (archivo != null) {
+				try {
+					archivo.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (archivo != null) {
-                try {
-                    archivo.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
 	}
-	
 
-
-	
 }
